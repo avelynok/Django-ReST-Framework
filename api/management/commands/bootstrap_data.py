f50f70f98252d5fa343a2650a022e0da25237245
avelyno <kou2295@calu.edu>
2020-06-30 15:23:55 -0400

    working properly

--- api/management/commands/bootstrap_data.py
+++ api/management/commands/bootstrap_data.py
@@ -2,35 +2,28 @@ from django.core.management.base import BaseCommand
 from shoe.models import ShoeColor, ShoeType
 
 class Command(BaseCommand):
-    help = 'populates ShoeColor and ShoeType tables with data.'
-    types = [
-        ('SK', 'Sneaker'),
-        ('B', 'Boot'),
-        ('SD', 'Sandal'),
-        ('D', 'Dress'),
-        ('O', 'Other')
-    ]
-    
-    colors = [
-        ('R', 'red'),
-        ('O', 'orange'),
-        ('Y', 'yellow'),
-        ('G', 'green'),
-        ('B', 'blue'),
-        ('I', 'indigo'),
-        ('V', 'violet'),
-        ('B', 'black'),
-        ('W', 'white'),
-    ]
-    
-    
+    help = 'populates Shoe Color and Shoe Type .'
     def handle(self, *args, **options):
+        types = [
+            ('SK', 'Sneaker'),
+            ('B', 'Boot'),
+            ('SD', 'Sandal'),
+            ('D', 'Dress'),
+            ('O', 'Other')
+        ]
+        colors = [
+            ('R', 'red'),
+            ('O', 'orange'),
+            ('Y', 'yellow'),
+            ('G', 'green'),
+            ('B', 'blue'),
+            ('I', 'indigo'),
+            ('V', 'violet'),
+            ('B', 'black'),
+            ('W', 'white'),
+        ]
         for color in colors:
-            ShoeColor.objects.create(
-                color_name = color
-            )
+            ShoeColor.objects.create(color_name = color)
         for type in types:
-            ShoeType.objects.create(
-                style = type
-            )
+            ShoeType.objects.create(style = type)
         
